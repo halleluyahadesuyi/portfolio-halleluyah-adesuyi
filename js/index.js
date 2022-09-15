602b33ac7b797929db1413908bfbba214c898116
Halleluyah <adesuyihalleluyah@gmail.com>
2022-09-15 21:52:54 +0100

    edit

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const hamburgerToggle = document.getElementsByClassName('hamburger-toggle')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 hamburgerToggle.addEventListener("click", () => {
-  navbarLinks.classList.add("active")
+    navbarLinks.classList.toggle("active")
 })
 
